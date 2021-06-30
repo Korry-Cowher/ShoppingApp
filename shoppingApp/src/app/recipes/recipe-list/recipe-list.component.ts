12b0022cf2c1cf45cc99d9676944c4ff1ebb0682
Korry-Cowher <KCowher14@yahoo.com>
2021-06-29 23:23:22 -0400

    added recipe model, implemented recipe list view

--- shoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://c.pxhere.com/photos/26/d0/zucchini_wraps_zucchini_slices_food_fish_fillet_recipe_fish_recipes_food_photography_plated_food_delicious_food-1376204.jpg!d'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://c.pxhere.com/photos/26/d0/zucchini_wraps_zucchini_slices_food_fish_fillet_recipe_fish_recipes_food_photography_plated_food_delicious_food-1376204.jpg!d')
+  ];
 
   constructor() { }
 
